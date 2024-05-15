df0598d88892fa2cd268c3a8f9bc467a52cdb392
jcampbell57 <jcampbell57@gmail.com>
2024-05-15 01:08:49 -0700

    create tests for capitalize

--- src/capitalize.test.js
+++ src/capitalize.test.js
@@ -1,6 +1,18 @@
 const capitalize = require('./capitalize');
 const string = 'random string'
 
-test('test1', () => {
+test('capitalize first letter', () => {
   expect(capitalize(string)).toBe('Random string');
+});
+
+test('empty string', () => {
+  expect(capitalize('')).toBe('');
+});
+
+test('null', () => {
+  expect(capitalize(null)).toBe(null);
+});
+
+test('undefined', () => {
+  expect(capitalize(undefined)).toBe(undefined);
 });
