ca8dc46707f515ab209e58c0611621e28f8c7b34
jcampbell57 <jcampbell57@gmail.com>
2024-05-18 01:23:03 -0700

    solve capitalize

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,3 +1,6 @@
-const capitalize = () => {
-
+const capitalize = (string) => {
+  if (!string) return string; // handle undefined or null
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+module.exports = capitalize;
