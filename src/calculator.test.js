17de464c4a13de3e5b7cc564fb373e72044665c5
jcampbell57 <jcampbell57@gmail.com>
2024-05-19 00:58:03 -0700

    create tests for calculator

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,25 +1,17 @@
-// class Calculator {
-//   static add(a, b) {
-//     return a + b
-//   }
-
-//   static subtract(a, b) {
-//     return a - b
-//   }
-
-//   static multiply(a, b) {
-//     return a * b
-//   }
-
-//   static divide(a, b) {
-//     return a / b
-//   }
-// }
-
-// export default Calculator
-
-const sum = require('./calculator');
+const Calculator = require('./calculator');
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+  expect(Calculator.sum(1, 2)).toBe(3);
+});
+
+test('subtracts 1 from 3 to equal 2', () => {
+  expect(Calculator.subtract(3, 1)).toBe(2);
+});
+
+test('multiplies 2 * 3 to equal 6', () => {
+  expect(Calculator.multiply(2, 3)).toBe(6);
+});
+
+test('divides 6 / 3 to equal 2', () => {
+  expect(Calculator.divide(6, 3)).toBe(2);
 });
