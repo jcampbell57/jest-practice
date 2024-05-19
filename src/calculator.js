936ca45c0c04f8c2f5cf3a0d23e3a2db71238eb1
jcampbell57 <jcampbell57@gmail.com>
2024-05-19 00:58:41 -0700

    create calculator class

--- src/calculator.js
+++ src/calculator.js
@@ -1,25 +1,19 @@
-// class Calculator {
-//   static add(a, b) {
-//     return a + b
-//   }
+class Calculator {
+  static sum(a, b) {
+    return a + b
+  }
 
-//   static subtract(a, b) {
-//     return a - b
-//   }
+  static subtract(a, b) {
+    return a - b
+  }
 
-//   static multiply(a, b) {
-//     return a * b
-//   }
+  static multiply(a, b) {
+    return a * b
+  }
 
-//   static divide(a, b) {
-//     return a / b
-//   }
-// }
-
-// export default Calculator
-
-function sum(a, b) {
-  return a + b;
+  static divide(a, b) {
+    return a / b
+  }
 }
 
-module.exports = sum;
+module.exports = Calculator;
