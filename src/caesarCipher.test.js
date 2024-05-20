e5ea40686272ef8d2e051a682a8b3e3a348e79bb
jcampbell57 <jcampbell57@gmail.com>
2024-05-20 13:18:49 -0700

    write tests for caeser cipher

--- src/caesarCipher.test.js
+++ src/caesarCipher.test.js
@@ -1,6 +1,10 @@
 const caesarCipher = require('./caesarCipher');
-const string = 'random string'
+const string = "What a string!"
 
-test('test1', () => {
-  expect(caesarCipher(string)).toBe('another random string');
+test('Caeser cipher #1', () => {
+  expect(caesarCipher(string, 5)).toBe('Bmfy f xywnsl!');
+});
+
+test('Caeser cipher #2', () => {
+  expect(caesarCipher(string, 6)).toBe('Cngz g yzxotm!');
 });
