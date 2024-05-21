8b7c655a3f880bc5ff8a88fe428d5f15d6e5d3bc
jcampbell57 <jcampbell57@gmail.com>
2024-05-21 12:45:00 -0700

    create tests for analizy array

--- src/analyzeArray.test.js
+++ src/analyzeArray.test.js
@@ -1,6 +1,15 @@
 const analyzeArray = require('./analyzeArray');
-const array = []
+const array = [1,8,3,4,2,6]
 
-test('test1', () => {
-  expect(analyzeArray(array)).toBe(1);
+test('Return object with stats', () => {
+  expect(analyzeArray(array)).toStrictEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6
+  });
+});
+
+test('Return empty object when given empty array', () => {
+  expect(analyzeArray([])).toStrictEqual({});
 });
