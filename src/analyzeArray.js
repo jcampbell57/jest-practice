2f01a1deebaf6c08fbd2f60397db8b19abde5b2b
jcampbell57 <jcampbell57@gmail.com>
2024-05-21 12:45:22 -0700

    complete analyze array

--- src/analyzeArray.js
+++ src/analyzeArray.js
@@ -1,3 +1,17 @@
-function analyzeArray() {
+function analyzeArray(arr) {
+  if (!arr.length) return {};
 
+  const min = Math.min(...arr);
+  const max = Math.max(...arr);
+  const length = arr.length;
+  const average = arr.reduce((sum, num) => sum + num, 0) / length;
+
+  return {
+    average: Math.round(average),
+    min: min,
+    max: max,
+    length: length
+  }
 }
+
+module.exports = analyzeArray;
